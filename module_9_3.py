790b0572cadfb8201a986bcd8763364f3e3c8b64
pola0noch <klayriddell@gmail.com>
2024-09-12 16:22:25 +0300

    init

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+# Домашнее задание по теме "Генераторные сборки"
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
+
+print(list(first_result))
+print(list(second_result))
